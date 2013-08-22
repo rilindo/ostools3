45b42ecc236afe1ed7d13176fc34247b4dedd1f9
jh6106 <jh6103.att@gmail.com>
2013-08-21 22:44:42 -0500

    UPDATE mysql test script for other db's

--- test_mysql_conn.py
+++ test_mysql_conn.py
@@ -3,34 +3,44 @@
 import ConfigParser
 import MySQLdb
 
-def get_nova_creds():
+def get_nova_creds(db):
     config = ConfigParser.ConfigParser()
     config.read('ostools.cfg')
 
-    name = config.get('novadb', 'name')
-    user = config.get('novadb', 'user')
-    password = config.get('novadb', 'pass')
-    host = config.get('novadb', 'host')
+    name = config.get(db, 'name')
+    user = config.get(db, 'user')
+    password = config.get(db, 'pass')
+    host = config.get(db, 'host')
 
     return (host,user,password,name)
 
-# Setup variables
-dbhost,dbuser,dbpass,dbname = get_nova_creds()
+def get_mysql_ver(db):
+    # Setup variables
+    dbhost,dbuser,dbpass,dbname = get_nova_creds(db)
 
-# Open the DB connection
-db = MySQLdb.connect(dbhost, dbuser, dbpass, dbname)
-cursor = db.cursor()
+    # Open the DB connection
+    db = MySQLdb.connect(dbhost, dbuser, dbpass, dbname)
+    cursor = db.cursor()
 
-# Execute the SQL command
-try:
-    cursor.execute("SELECT version()")
-    results = cursor.fetchone()
-except:
-    print "Error: unable to fetch version"
-    sys.exit(2)
+    # Execute the SQL command
+    try:
+        cursor.execute("SELECT version()")
+        results = cursor.fetchone()
+    except:
+        print "Error: unable to fetch version"
+        sys.exit(2)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
+    return results[0]
 
-print("MySQL Version: %s" % results[0] )
+nova = get_mysql_ver('novadb')
+kstn = get_mysql_ver('keystonedb')
+#cndr = get_mysql_ver('cinderdb')
+#glnc = get_mysql_ver('glancedb')
+
+print("    Nova MySQL Version: %s" % nova )
+print("Keystone MySQL Version: %s" % kstn )
+#print("  Cinder MySQL Version: %s" % cndr )
+#print("  Glance MySQL Version: %s" % glnc )
 
